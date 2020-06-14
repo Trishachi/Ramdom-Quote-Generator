722f16985303b43a8b101cf3b1af001f4a01b6e4
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-14 01:44:16 -0600

    Refactored Js file

--- main.js
+++ main.js
@@ -1,56 +1,48 @@
 const url =
   "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-let request = new XMLHttpRequest();
 
-request.onload = function () {
-  // begin accessing JSON data here
-  var data = JSON.parse(this.responseText);
-  getQuote(data);
-};
-request.open("GET", url, true);
-request.send();
+let quote = document.getElementById("text");
+let author = document.getElementById("author");
+let tweet = document.getElementById("tweet-quote");
+let newQuote = document.getElementById("new-quote");
 
 function getRandomIndex(range) {
   let index = Math.floor(Math.random() * range + 1);
   return index;
 }
 
-function getQuote(data) {
-  let saying = data.quotes[getRandomIndex(data.quotes.length)];
-  //   console.log(saying);
-  let text = saying.quote;
-  let author = saying.author;
-  document.getElementById("text").innerText = text;
-  document.getElementById("author").innerText = author;
-  document
-    .getElementById("tweet-quote")
-    .setAttribute(
-      "href",
-      "https://twitter.com/intent/tweet?text=" +
-        '"' +
-        text +
-        '"' +
-        " - " +
-        author
-    );
-}
-
-function handleNewQuote(evt) {
-  evt.preventDefault();
+function getQuote() {
   let request = new XMLHttpRequest();
   request.onreadystatechange = function () {
-    var data = JSON.parse(this.responseText);
-    getQuote(data);
+    if (this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(this.responseText);
+      let saying = data.quotes[getRandomIndex(data.quotes.length)];
+      let text = saying.quote;
+      let saidBy = saying.author;
+      quote.innerText = text;
+      author.innerText = saidBy;
+      tweet.setAttribute(
+        "href",
+        "https://twitter.com/intent/tweet?text=" +
+          '"' +
+          text +
+          '"' +
+          " - " +
+          saidBy
+      );
+    }
   };
   request.open("GET", url, true);
   request.send();
 }
 
-function handleTweetButton(evt) {
-  console.log("Tweet content");
+window.onload = (event) => {
+  getQuote();
+};
+
+function handleNewQuote(event) {
+  event.preventDefault();
+  getQuote();
 }
 
-document.getElementById("new-quote").addEventListener("click", handleNewQuote);
-document
-  .getElementById("tweet-quote")
-  .addEventListener("click", handleTweetButton);
+newQuote.addEventListener("click", handleNewQuote);
